4296fdd507f0bc362be1520a451ac0a3e84af80f
Jhessica Pinedo <jhessicapinedo@gmail.com>
2025-11-24 00:55:24 -0500

    Update metods succes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,40 @@
 // /app/layout.tsx
 import type { Metadata } from "next";
-import { Inter, Poppins, Rubik,Jost } from "next/font/google"; // Usamos Rubik como pediste antes
+import { Inter, Poppins, Rubik, Jost } from "next/font/google"; // Usamos Rubik como pediste antes
 import "./globals.css";
 import Sidebar from "@/components/layout/Sidebar";
 import { getHomePageData } from "@/lib/strapi";
 import WhatsAppButton from "@/components/ui/WhatsAppButton";
-import { CartProvider } from "@/lib/cart-context";
-import { CartModal } from "@/components/cart/CartModal";
 
-const inter = Inter({ 
+const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
 });
 
-const jost= Jost({
-
-  weight: ["100", "200", "300","400", "500", "700", "800", "900"], 
+const jost = Jost({
+  weight: ["100", "200", "300", "400", "500", "700", "800", "900"],
   variable: "--font-jost",
 });
 
-
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["200", "500", "700", "800"], 
+  weight: ["200", "500", "700", "800"],
   variable: "--font-poppins",
 });
 
 // Ajustamos Titan One/Rubik según tu preferencia
 const titan = Rubik({
   subsets: ["latin"],
-  weight: ["900"], 
+  weight: ["900"],
   variable: "--titan-one-regular", // Mantenemos el nombre de variable que usas en Hero
 });
 
 export const metadata: Metadata = {
-  title: "H2GO - Innovación en Hidratación",
+  title: "h2go - Innovación en Hidratación",
   description: "Descubre la innovadora botella de agua.",
-  icons:{
-    icon: "/logo-transparente.ico",
-  }  
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default async function RootLayout({
@@ -46,7 +42,6 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
   let logoUrl = null;
   let siteTitle = "GOH2"; // Valor por defecto
   try {
@@ -56,25 +51,26 @@ export default async function RootLayout({
     }
 
     // 2. Obtenemos el título
-      if (homeData.title) {
-        siteTitle = homeData.title;
-      }
+    if (homeData.title) {
+      siteTitle = homeData.title;
+    }
   } catch (error) {
     console.error("Error cargando el logo:", error);
   }
 
   return (
     <html lang="es" className="scroll-smooth">
-      <body className={`${inter.variable} ${poppins.variable} ${titan.variable} ${jost.variable} bg-gray-900 text-white antialiased overflow-x-hidden`}>
-        <CartProvider>
-          <Sidebar logoUrl={logoUrl}  siteTitle={siteTitle}/>
-          <main className="w-full min-h-screen relative">
-              {children}
-          </main>
-          <CartModal />
-          <WhatsAppButton />
-        </CartProvider>
+      <body
+        className={`${inter.variable} ${poppins.variable} ${titan.variable} ${jost.variable} bg-gray-900 text-white antialiased overflow-x-hidden`}
+      >
+       
+        <Sidebar logoUrl={logoUrl} siteTitle={siteTitle} />
+        <main className="w-full min-h-screen relative">
+          {children}
+        </main>
+
+        <WhatsAppButton />
       </body>
     </html>
-  );
+);
 }
